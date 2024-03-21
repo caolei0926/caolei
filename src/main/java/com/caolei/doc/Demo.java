e4460a80fe7e89e9126b5573b336deec9d862781
caolei <caolei0926.com>
2024-03-21 10:32:16 +0800

    练习

---
+++ src/main/java/com/caolei/doc/Demo.java
@@ -0,0 +1,8 @@
+package com.caolei.doc;
+
+/**
+ * @author 曹磊
+ */
+public class Demo {
+    DocDemo demo = new DocDemo();
+}
---
+++ src/main/java/com/caolei/doc/DocDemo.java
@@ -0,0 +1,10 @@
+package com.caolei.doc;
+
+/**
+ * @author 曹磊
+ * @version 1.0
+ * @since 1.0
+ * @see java.lang.String
+ */
+public class DocDemo {
+}
