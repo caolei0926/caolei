905608c7e827687caa928f664d9bd725f84cf0a6
caolei <caolei0926.com>
2024-03-21 17:38:38 +0800

    练习

---
+++ src/main/java/com/caolei/string/ToUpperCase.java
@@ -0,0 +1,13 @@
+package com.caolei.string;
+
+/**
+ * ToUpperCase全转大写
+ * ToLowerCase全转小写
+ */
+public class ToUpperCase {
+    public static void main(String[] args) {
+        String s1 = "FSSae123abcJaVA";
+        System.out.println(s1.toUpperCase());
+        System.out.println(s1.toLowerCase());
+    }
+}
