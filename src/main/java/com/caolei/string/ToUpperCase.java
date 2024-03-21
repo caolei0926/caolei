84d69a1703daf3c4746fd985bba411a90ab13ca9
caolei <caolei0926.com>
2024-03-21 17:50:54 +0800

    练习

--- src/main/java/com/caolei/string/ToUpperCase.java
+++ src/main/java/com/caolei/string/ToUpperCase.java
@@ -1,5 +1,7 @@
 package com.caolei.string;
 
+import java.util.Scanner;
+
 /**
  * ToUpperCase全转大写
  * ToLowerCase全转小写
@@ -9,5 +11,13 @@ public class ToUpperCase {
         String s1 = "FSSae123abcJaVA";
         System.out.println(s1.toUpperCase());
         System.out.println(s1.toLowerCase());
+
+
+        String s2 = "FSSae";
+        System.out.println("请输入验证码：FSSae");
+        Scanner scanner = new Scanner(System.in);
+        String s3 = scanner.nextLine();
+        System.out.println(s2.toUpperCase().equals(s3.toUpperCase()));
+        System.out.println(s2.equalsIgnoreCase(s3));
     }
 }
