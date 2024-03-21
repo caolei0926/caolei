88fa2e9f2427ada864e8deca86c3ba11dfacb794
caolei <caolei0926.com>
2024-03-21 14:08:36 +0800

    练习

--- src/main/java/com/caolei/string/String_SE.java
+++ src/main/java/com/caolei/string/String_SE.java
@@ -7,6 +7,6 @@ public class String_SE {
     public static void main(String[] args) {
         String s1 = "123abc";
         String s2 = "123abc";
-        System.out.println(s1 == s2);
+        System.out.println(s1.equals(s2));
     }
 }
