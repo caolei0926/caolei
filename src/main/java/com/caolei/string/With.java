f17529ac23450d48826ca3c10052ea4a1b92705f
caolei <caolei0926.com>
2024-03-21 17:23:49 +0800

    练习

---
+++ src/main/java/com/caolei/string/With.java
@@ -0,0 +1,13 @@
+package com.caolei.string;
+
+/**
+ * startsWith判断是否以什么开始
+ * endsWith判断是否以什么结束
+ */
+public class With {
+    public static void main(String[] args) {
+        String str = "www.baidu.com";
+        System.out.println(str.startsWith("www"));
+        System.out.println(str.endsWith("cOm"));
+    }
+}
